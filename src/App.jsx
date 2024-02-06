c8f67d1f9583a98f419851a6b6b972e20812e3af
lulb11 <bordeslucas11@gmail.com>
2024-02-06 11:19:35 +0100

    tailwind work

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-function App() {
-  return <div>app</div>;
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
 }
-
-export default App;
